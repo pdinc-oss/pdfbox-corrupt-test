1f932a83f1d87f61d37eb054bb6444f1220c09eb
Alex Slonim <aslonim@pdinc.us>
2021-07-28 15:25:38 -0400

    bug 2467 now using PDFbox 3.0.1, using non-dd2875pdf added a few more tests cases

--- src/main/java/CorruptFilesTest.java
+++ src/main/java/CorruptFilesTest.java
@@ -1,26 +1,30 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.junit.jupiter.api.Test;
 
-
 public class CorruptFilesTest
 {
     @Test
-    public void corruptedThroughFileOutputStream() throws InvalidPasswordException, IOException {
-        InputStream fis = this.getClass().getResourceAsStream("DD_2875.pdf");
+    public void corruptedThroughFileOutputStream() throws IOException {
+
+        InputStream fis = this.getClass().getResourceAsStream("chicken.pdf");
         if (fis == null)
         {
             throw new NullPointerException();
         }
-        
-        PDDocument document = PDDocument.load(fis);
-        PDStream stream = new PDStream(document);
+        PDDocument document = Loader.loadPDF(fis);
+        PDStream stream = new PDStream(document, fis);
+
+        fis.close();
         byte[] bytes = stream.toByteArray();
         File tempFile = File.createTempFile("fosCorrupted", ".pdf");
         FileOutputStream fos = new FileOutputStream(tempFile);
@@ -32,15 +36,29 @@ public class CorruptFilesTest
 
     @Test
     public void corruptedThroughDocSave() throws InvalidPasswordException, IOException {
-        InputStream fis = this.getClass().getResourceAsStream("DD_2875.pdf");
+
+        InputStream fis = this.getClass().getResourceAsStream("chicken.pdf");
         if (fis == null)
         {
             throw new NullPointerException();
         }
-        PDDocument document = PDDocument.load(fis);
+        PDDocument document = Loader.loadPDF(fis);
+        fis.close();
 
-        File tempFile = File.createTempFile("saveCorrupted", ".pdf");
+        File tempFile = new File("C:\\Users\\Public\\Music\\file" + new Date().getTime() + ".pdf");
         document.save(tempFile);
+        document.close();
         System.out.println(tempFile.getAbsolutePath());
+    }
+
+    @Test
+    public void loadFileAndSave() throws IOException {
+
+        File fil = new File("C:\\Users\\Public\\Desktop\\projects\\test-pdfbox-corruption\\src\\main\\resources\\chicken.pdf");
+        PDDocument document = Loader.loadPDF(fil);
+
+        document.save("C:\\Users\\Public\\Music\\file" + new Date().getTime() + ".pdf");
+        document.close();
+
     }
 }
